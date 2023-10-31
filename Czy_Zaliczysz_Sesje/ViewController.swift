77d5435453155fde30ea434da092ac35cafa978b
Rafal Glogowski <studia@glogo.pl>
2023-10-31 12:31:02 +0100

    Initial Commit

---
+++ Czy_Zaliczysz_Sesje/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Czy_Zaliczysz_Sesje
+//
+//  Created by Rafal Glogowski on 31/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
